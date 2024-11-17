b42daa978f24a8cafa6c807bddc0c77c6c1f14ba
Ezequiel-Manfredi <ezemanfre@gmail.com>
2024-11-17 17:51:30 -0300

    add: initial account model

---
+++ quick_transfer/apps/account/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from apps.user.models import User
+
+class Account(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+  balance = models.FloatField(default=0.00)
+  favorites = models.ManyToManyField('self')
