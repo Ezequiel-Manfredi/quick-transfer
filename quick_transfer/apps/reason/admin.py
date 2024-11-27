f069626ea9bfb4b5ad59e7996ebf77ebc9884391
Ezequiel-Manfredi <ezemanfre@gmail.com>
2024-11-27 00:28:54 -0300

    add: admin and url file

--- quick_transfer/apps/reason/admin.py
+++ quick_transfer/apps/reason/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Reason
 
-# Register your models here.
+admin.site.register(Reason)
--- quick_transfer/apps/transaction/models.py
+++ quick_transfer/apps/transaction/models.py
@@ -3,8 +3,8 @@ from apps.account.models import Account
 from apps.reason.models import Reason
 
 class Transaction(models.Model):
-  emitter = models.ForeignKey(Account, on_delete=models.CASCADE)
-  receiver = models.ForeignKey(Account, on_delete=models.CASCADE)
+  emitter = models.ForeignKey(Account, related_name='transactions_sent', on_delete=models.CASCADE)
+  receiver = models.ForeignKey(Account, related_name='transactions_received', on_delete=models.CASCADE)
   amount = models.FloatField(default=0.00)
   date = models.DateTimeField(auto_now_add=True)
   reason = models.ForeignKey(Reason,on_delete=models.CASCADE)
